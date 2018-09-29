3f094a98eda48c740c08e8157b07fc1fa49c429f
vagrant <vagrant@homestead>
2018-09-29 09:08:48 +0000

    User profile page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,11 +7,13 @@
 
 	<!-- CSRF Token -->
 	<meta name="csrf-token" content="{{ csrf_token() }}">
+
 	<title>@yield('title','LaraBBS') - Laravel 进阶教程</title>
 
 	<!-- Styles -->
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
+
 <body>
 	<div id="app" class="{{ route_class() }}-page">
 		
